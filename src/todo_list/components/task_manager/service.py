b366260a12426921cefb3e8a5b97a8984c80c805
Khushi Bansal <bansalkhushi880@gmail.com>
2026-03-23 11:38:03 +0530

    Add Redis caching

--- src/todo_list/components/task_manager/service.py
+++ src/todo_list/components/task_manager/service.py
@@ -1,5 +1,7 @@
 from . import repository, auth
 from . import models, schemas
+import json
+from todo_list.redis_client import redis_client  # ← Redis import karo
 
 
 def register_user_service(db, user):
@@ -16,27 +18,71 @@ def send_otp_service(username):
     return auth.generate_otp(username)
 
 
-def verify_otp_service(db, username, otp):      
+def verify_otp_service(db, username, otp):
     if not auth.verify_otp(username, otp):
         return None
 
-    user = repository.get_user(db, username)  
-    
-    token = auth.create_token({"sub": user.username, "role": user.role})  
+    user = repository.get_user(db, username)
+
+    token = auth.create_token({"sub": user.username, "role": user.role})
     return token
 
 
 def create_task_service(db, task, current_user):
-    if current_user["role"] not in ["admin", "manager", "user"]:  
+    if current_user["role"] not in ["admin", "manager", "user"]:
         raise Exception("Not allowed to create task")
-    return repository.create_task(db, task)
+    
+    new_task = repository.create_task(db, task)
+    
+    # ⚠️ Naya task bana — purana cache delete karo
+    redis_client.delete("tasks:all")
+    
+    return new_task
 
 
 def get_tasks_service(db, current_user):
-    return repository.get_tasks(db)
+    
+    
+    cached = redis_client.get("tasks:all")
+    
+    if cached:
+       
+        print("✅ Redis se mila!")
+        return json.loads(cached)  
+    
+    
+    print("🔄 DB se le raha hun...")
+    tasks = repository.get_tasks(db)
+    
+  
+    tasks_data = [
+        {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "completed": task.completed,
+            "owner_id": task.owner_id
+        }
+        for task in tasks
+    ]
+    
+    
+    redis_client.setex(
+        "tasks:all",        # ← Key
+        300,                # ← 5 minutes
+        json.dumps(tasks_data)  # Python List → String
+    )
+    
+    return tasks_data
 
 
 def delete_task_service(db, task_id, current_user):
     if current_user["role"] != "admin":
         raise Exception("Only admin can delete")
-    return repository.delete_task(db, task_id)
+    
+    result = repository.delete_task(db, task_id)
+    
+    
+    redis_client.delete("tasks:all")
+    
+    return result
