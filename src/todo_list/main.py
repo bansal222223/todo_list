63c1b5adb182cb32d915a9319324b6e471ca31f5
Khushi Bansal <bansalkhushi880@gmail.com>
2026-03-27 12:37:08 +0530

    remove create_all for CI fix

--- src/todo_list/main.py
+++ src/todo_list/main.py
@@ -2,16 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from todo_list.components.task_manager.controller import router as task_router
-from todo_list.components.task_manager import models
-from .database import Base, engine
 
 app = FastAPI()
-
-
-# ✅ ONLY RUN ON STARTUP
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
 
 
 app.add_middleware(
